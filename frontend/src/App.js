ed37929013cf217402ab3687207671435d6e35ae
brandon <brandon.w.leung@gmail.com>
2023-04-23 23:00:56 -0700

    reroute unknown urls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import LandingPage from "./components/LandingPage";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <>
       {/* <h1>Hello from App</h1> */}
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/login" component={LoginForm} />
-      <Route path="/signup" component={LoginForm}/>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/signup" component={LoginForm}/>
+        <Redirect to="/login" />
+      </Switch>
+      <Route path='/' component={Footer} />
     </>
  
 
--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,16 @@
 import './Footer.css';
+import linkedinLogo from '../../assets/LI-In-Bug.png'
+import githubLogo from '../../assets/github-mark.png'
 
 const Footer = () => {
     return (
         <footer>
-            footer
+            <a href="https://github.com/bdongo" target="_blank">
+                <img src={githubLogo} />
+            </a>
+            <a href="https://www.linkedin.com/in/brandon-l/" target="_blank">
+                <img src={linkedinLogo}/>
+            </a>   
         </footer>
     )
        
