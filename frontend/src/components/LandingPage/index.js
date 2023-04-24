04e3c31a84bdb3d856b4d629436e97c68b94cc61
brandon <brandon.w.leung@gmail.com>
2023-04-24 10:47:26 -0700

    fixing navbar component for sign in and sign up

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -4,31 +4,37 @@ import './LandingPage.css'
 import { getCurrentUser, showCurrentUser } from '../../store/session'
 import { Link } from 'react-router-dom/cjs/react-router-dom'
 import { useEffect } from 'react'
+import NavBar from '../NavBar'
 
 const LandingPage = () => {
     const dispatch = useDispatch();
     const currentUser = useSelector(getCurrentUser)
-    console.log("in landing page")
-    console.log(currentUser)
-
-    // useEffect(() => {
-    //     dispatch(showCurrentUser())
-    // }, [dispatch, currentUser])
 
     return (
-        <>
-            <h1>Landing Page</h1>
+        <div>
+            <div>
+                <NavBar></NavBar>
+            </div>
+
+            <div id="image-container">
+            </div>
 
             {currentUser && <UserGreeting currentUser={currentUser} />}
 
-            {!currentUser && 
-                    <Link to='/login'><button>Log In</button></Link>
-             
-            }
+            {/* {!currentUser &&
+                <ul>
+                    <li>
+                        <Link to='/login'><button>Log In</button></Link>
+                    </li>
+                    <li>
+                        <Link to='/signup'><button>Sign Up</button></Link>
+                    </li>
+                </ul>
+            } */}
 
 
             
-        </>
+        </div>
     )
 }
 
--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
 import logo from '../../assets/logo.png';
+import { getCurrentUser } from '../../store/session'
+import { useSelector } from 'react-redux'
+import UserGreeting from '../UserGreeting'
 
 const NavBar = () => {
+    const currentUser = useSelector(getCurrentUser)
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    let navDisplay = true;
+    if (currentUser || currentPath === '/login' || currentPath === '/signup') {
+        navDisplay = false;
+    }
 
     return (
         <header id="header">
@@ -10,6 +21,19 @@ const NavBar = () => {
                 <Link to="/">
                     <img class="logo" src={logo} alt="logo" />
                 </Link>
+
+                {navDisplay && <UserGreeting currentUser={currentUser} />}
+
+                {navDisplay &&
+                    <ul id='action-container'> 
+                        <li>
+                            <Link to='/login'><button className="clear-button nav-button">Log In</button></Link>
+                        </li>
+                        <li>
+                            <Link to='/signup'><button className='red-button nav-button'>Sign Up</button></Link>
+                        </li>
+                    </ul>
+                }
             </div>
         </header>
     )
