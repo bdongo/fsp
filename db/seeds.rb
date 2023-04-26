9a3d42629a904c7c9bb8395d22f8bc7c7dc3919e
brandon <brandon.w.leung@gmail.com>
2023-04-26 14:45:12 -0700

    for deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.delete_all
+BusinessPage.delete_all
+
 
 ua = User.create!(
         f_name: "John",
