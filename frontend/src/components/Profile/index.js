2d783bdf421b0d2a6a4cd86833cd7f287f71bfe5
brandon <brandon.w.leung@gmail.com>
2023-04-26 17:33:36 -0700

    profile icon css

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -2,9 +2,26 @@ import { useDispatch, useSelector } from "react-redux"
 import "./Profile.css"
 import profilePic from "../../assets/user.png"
 import { logout } from "../../store/session";
+import { useRef, useState, useEffect } from "react";
 
 const Profile = ({currentUser}) => {
     const dispatch = useDispatch();
+    const [open, setOpen] = useState(false);
+    const dropDownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (dropDownRef.current && !dropDownRef.current.contains(e.target)) {
+                setOpen(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+    }, [dropDownRef])
 
     if (currentUser === null) {
         return null;
@@ -13,18 +30,22 @@ const Profile = ({currentUser}) => {
     if (sessionStorage.getItem('CurrentUser') === null) {
         return null;
     }
-    
+
     return (
         <>
-        <div class="name-text">
-            <span className="name-text-tooltip" >
+            <div class="name-text" ref={dropDownRef} onClick={() => setOpen(!open)}>
+            <span className={open ? "name-text-tooltip clicked" : "name-text-tooltip"} >
                 {currentUser.fName} {currentUser.lName}
             </span>
             <img src={profilePic} />
+            {open && 
+                <div className="dropdown">
+                    <a onClick={() => dispatch(logout())} >
+                        Log out
+                    </a>
+                </div>
+            }
         </div>
-
-            
-            <button onClick={() => dispatch(logout())} >Log Out</button>
         </>
     )
 
