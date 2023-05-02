aa8a5bc31cba0a798641843334d3c3ebcae61643
brandon <brandon.w.leung@gmail.com>
2023-05-02 11:06:48 -0700

    pricing defaults to 2

--- db/migrate/20230427044653_swap_price_column.rb
+++ db/migrate/20230427044653_swap_price_column.rb
@@ -1,7 +1,7 @@
 class SwapPriceColumn < ActiveRecord::Migration[7.0]
   def change
     remove_column :reviews, :pricing 
-    add_column :business_pages, :pricing, :integer
+    add_column :business_pages, :pricing, :integer, default: 2
     change_column_null :business_pages, :pricing, false
   end
 end
